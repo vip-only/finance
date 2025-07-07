0d12a4f1ac32bae949834152efd90d0a9282f5fc
vip-only <safidymirantsoar@gmail.com>
2025-07-07 23:18:58 +0300

    update index

--- ws/index.php
+++ ws/index.php
@@ -5,5 +5,5 @@ require 'routes/clients_routes.php';
 require 'routes/pret_routes.php';
 require 'routes/fonds_routes.php';
 require 'routes/TypePret.php';
-require 'routes/TypePret.php';
+
 Flight::start();
