780febe8497a1f781985cf1b5dd7736d0f60a12a
vip-only <safidymirantsoar@gmail.com>
2025-07-07 16:55:29 +0300

    update db

--- ws/db.php
+++ ws/db.php
@@ -1,9 +1,9 @@
 <?php
 function getDB() {
     $host = 'localhost';
-    $dbname = 'tp_flight';
+    $dbname = 'banque';
     $username = 'root';
-    $password = 'root';
+    $password = '';
 
     try {
         return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
